a0e0d28c0395a50ec962972b558a2f96ee992e7f
islishude <islishude@gmail.com>
2020-03-13 13:37:02 +0800

    test: make sure word list's length always 2048

--- lang_test.go
+++ lang_test.go
@@ -66,7 +66,7 @@ func TestLanguage_List(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.lan.list(); !reflect.DeepEqual(got, tt.want) {
+			if got := tt.lan.list(); !reflect.DeepEqual(got, tt.want) || len(got) != 2048 {
 				t.Errorf("Language.List() = %v, want %v", got, tt.want)
 			}
 		})
@@ -91,8 +91,12 @@ func TestLanguage_mapping(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.lan.mapping(); !reflect.DeepEqual(got, tt.want) {
+			got := tt.lan.mapping()
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Language.mapping() = %v, want %v", got, tt.want)
+			}
+			if got != nil && len(got) != 2048 {
+				t.Errorf("Language.mapping() wants 2048 elements but got %d", len(got))
 			}
 		})
 	}
